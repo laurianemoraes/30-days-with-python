989a249a81d5ca56783a93f90d5b26be4557e26b
Lauriane <lauriane.moraes10@gmail.com>
2022-08-27 23:14:28 -0400

    Learn Python - Full Course for Beginners [Tutorial]
    first exercise

---
+++ name.py
@@ -0,0 +1,6 @@
+characte_name= "Ana"
+characte_age= "20"
+print("There once was a girl call "+ characte_name +" that wish to fly ")
+print("she has "+ characte_age + " years old. ")
+print("she loves her name  "+ characte_name + ", ")
+print("but didn't like being "+ characte_age +".")
