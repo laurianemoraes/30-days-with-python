9101895870e6a4d793169f060882c518fd86e2be
Lauriane <lauriane.moraes10@gmail.com>
2022-08-28 21:55:23 -0400

    exercise in order to learn basic functions on python

---
+++ functions.py
@@ -0,0 +1,8 @@
+phrase = " Python in 30 days"
+print(phrase.upper())
+print(phrase.lower().islower())
+print(phrase[4])
+print(len(phrase))
+print(phrase.index("30"))
+print(phrase.index("3"))
+print(phrase.replace("30","DOG"))
