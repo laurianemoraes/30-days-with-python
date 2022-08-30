d3b67df12202ad179c872c391065e04100329a9e
Lauriane <lauriane.moraes10@gmail.com>
2022-08-29 22:02:14 -0400

    receive the informtion from the user

---
+++ hiUser.py
@@ -0,0 +1,7 @@
+from unicodedata import name
+
+
+name = input("Enter your name : ")
+print("Hi "+ name + "!! welcome to my page ")
+question = input("Where you can from? ")
+print("Great " +name + " I'm happy to now that you are from "+ question)
