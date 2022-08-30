1703949a1bb2fcbdef14a1de4e10a3da79e7c654
Lauriane <lauriane.moraes10@gmail.com>
2022-08-29 22:02:55 -0400

    basic operation with numbers

---
+++ numbers.py
@@ -0,0 +1,12 @@
+from math import *
+
+my_num = 5
+print(pow(3, 6))
+print(max(4, 9, 22))#maximo
+print(round(8.08))#arredonda
+print(floor(9.98))#menor(piso)
+print(ceil(9.98))#teto
+print(sqrt(9.98))
+
+
+
