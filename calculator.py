c6b329804e6f1046487389c04faaf73868ccd700
Lauriane <lauriane.moraes10@gmail.com>
2022-08-30 23:19:53 -0400

    simple calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#sum
+from symbol import term
+
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another  number: ")
+result=float(num1) + float(num2) #acept decimal numbers
+print(result)
+
+
+
