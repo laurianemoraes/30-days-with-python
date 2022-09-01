5d54c98f7aa63532e0aa2a80eb05ee84b93345ba
Lauriane <lauriane.moraes10@gmail.com>
2022-09-01 00:03:10 -0400

    initiation on list

---
+++ lists.py
@@ -0,0 +1,8 @@
+fruits=["apple", "strawberry", "orange", "lemon"]
+fruits[2]="banana"#add a new valor to the variable 2
+print(fruits)#print the list
+print(fruits[2])
+print(fruits[-1])#regressive order
+print(fruits[1:])#exclud the refer element
+print(fruits[1:3])#gonna exclude the 3 position element
+fruits[2]="banana"#add a new valor to the variable 2
