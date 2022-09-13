b8d7e27e5bbd399abb5b41db8c1b9c963ff0c052
Lauriane <lauriane.moraes10@gmail.com>
2022-09-12 23:32:22 -0400

    dictionary project

---
+++ dictionairies.py
@@ -0,0 +1,17 @@
+monthConvertions= {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December",
+    }
+
+#print(monthConvertions["Jun"])
+print(monthConvertions.get("Dec"))
