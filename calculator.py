3ba654ae2e754d53c1380e3741e904937a92a513
Lauriane <lauriane.moraes10@gmail.com>
2022-09-12 23:32:47 -0400

    update calculator

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ from symbol import term
 
 num1 = input("Enter a number: ")
 num2 = input("Enter another  number: ")
-result=float(num1) + float(num2) #acept decimal numbers
+result = float(num1) + float(num2) #float acept decimal numbers
 print(result)
 
 
